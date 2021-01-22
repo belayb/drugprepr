d087806e21628f5644bd03d7f7d68362c68a4b58
belayb <belaybirlie.yimer@manchester.ac.uk>
2021-01-22 09:43:59 +0000

    call doseminer update

--- R/call_dosemine.R
+++ R/call_dosemine.R
@@ -9,4 +9,13 @@
 call_dosemine<-function(dataset1=NULL, decisions=NULL)
 {
 
+  # crate a dataset from dataset1 with unique dossageid
+  temp_presc<-dataset1[!duplicated(dataset1[,c(dossageid)]),
+                                           c("dossageid","text","dose_number","dose_unit","dose_frequency",
+                                             "dose_interval","choice_of_dose","dose_max_average", "change_dose")]
+  temp_presc<-doseminer::extract_from_prescription(temp_presc)
+  dataset1<-left_join(dataset1,temp_presc,by="dossageid")
+
+  return(dataset1)
+
 }
