7dfeac5e344fa4d70348cc04dcbdf17fe4b3ee9f
belayb <belaybirlie.yimer@manchester.ac.uk>
2021-06-07 15:49:50 +0100

    Cleaned documentation  for R CMD check
    I have renamed the call_dosemine function in to compute_ndd and only the selected ndd is computed avoiding the creation of multiple columns

--- R/implausible_values.R
+++ R/implausible_values.R
@@ -1,6 +1,6 @@
 #' Define Implausible
 #'
-#'This function flag quantity and ndd informations that are beyond the acceptable levels in the dataset. The acceptable level is obtained from NICE BNF.
+#' This function flag quantity and ndd informations that are beyond the acceptable levels in the dataset. The acceptable level is obtained from NICE BNF.
 #'
 #' @param dataset1 a data frame object containing the prescription information
 #' @param min_max_dat a data frame object containing minimum and maximum possible values for each drug
@@ -11,16 +11,21 @@
 #'
 #' @export
 
-Implausible_values<-function(dataset1=NULL,min_max_dat=NULL)
-{
-  if(is.null(dataset1)) {stop("\n Data set with information from therapy file of CPRD has to be supplimented")}
-  if(is.null(min_max_dat)) {stop("\n Data set with information including qty_max, qty_min, min_rec_ndd, max_rec_ndd to be supplimented")}
+Implausible_values <- function(dataset1 = NULL, min_max_dat = NULL) {
+  if (is.null(dataset1)) {
+    stop("\n Data set with information from therapy file of CPRD has to be supplimented")
+  }
+  if (is.null(min_max_dat)) {
+    stop("\n Data set with information including qty_max, qty_min, min_rec_ndd, max_rec_ndd to be supplimented")
+  }
 
-  dataset1<-dplyr::left_join(dataset1,min_max_dat, by="drugsubstance")
-  dataset1<-dataset1%>%
+  dataset1 <- dplyr::left_join(dataset1, min_max_dat, by = "drugsubstance")
+  dataset1 <- dataset1 %>%
     dplyr::rowwise() %>%
-    dplyr::mutate(implausible_qty=(qty>qty_max|qty<qty_min)&!is.na(qty),
-                  implausible_ndd=(ndd>max_rec_ndd|ndd<min_rec_ndd)&!is.na(ndd))%>%
+    dplyr::mutate(
+      implausible_qty = (qty > qty_max | qty < qty_min) & !is.na(qty),
+      implausible_ndd = (ndd > max_rec_ndd | ndd < min_rec_ndd) & !is.na(ndd)
+    ) %>%
     dplyr::ungroup()
 
   return(dataset1)
