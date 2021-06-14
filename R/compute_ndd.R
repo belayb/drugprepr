569712a98b5837af35b2da7a7bc70dc4356f7f8a
Selby <david@selbys.org.uk>
2021-06-14 10:48:59 +0100

    Simplify compute_ndd function code

--- R/compute_ndd.R
+++ R/compute_ndd.R
@@ -9,22 +9,24 @@
 #' minimum or maximum or average of value of DN or DF.
 #'
 #'
-#' @param dataset1 a data frame object containing the prescription information.
+#' @param data a data frame object containing the prescription information.
 #' The dataset is must have column names 'dosageid' and 'text'. This dataset is
 #' expected to be a result of merging CPRD 'therapy' table with 'commondosage' table.
 #' @param decision a character variable for computing number of (ndd).
-##' \itemize{
-##' \item{"min_min"}{ minimum dose number and minimum dose frequency}
-##' \item{"min_mean"}{ minimum dose number and mean dose frequency}
-##' \item{"min_max"}{ minimum dose number and maximum dose frequency}
-##' \item{"mean_min"}{ mean dose number and minimum dose frequency}
-##' \item{"mean_mean"}{ mean dose number and mean dose frequency}
-##' \item{"mean_max"}{ mean dose number and maximum dose frequency}
-##' \item{"max_min"}{ maximum dose number and minimum dose frequency}
-##' \item{"max_mean"}{ maximum dose number and mean dose frequency}
-##' \item{"max_max"}{ maximum dose number and maximum dose frequency}
-##' }
-##'
+#' \itemize{
+#' \item{"min_min"}{ minimum dose number and minimum dose frequency}
+#' \item{"min_mean"}{ minimum dose number and mean dose frequency}
+#' \item{"min_max"}{ minimum dose number and maximum dose frequency}
+#' \item{"mean_min"}{ mean dose number and minimum dose frequency}
+#' \item{"mean_mean"}{ mean dose number and mean dose frequency}
+#' \item{"mean_max"}{ mean dose number and maximum dose frequency}
+#' \item{"max_min"}{ maximum dose number and minimum dose frequency}
+#' \item{"max_mean"}{ maximum dose number and mean dose frequency}
+#' \item{"max_max"}{ maximum dose number and maximum dose frequency}
+#' }
+#'
+#' @examples
+#' compute_ndd(dataset1, "min_min")
 #'
 #' @return Dataframe with the same structure as the input with additional column named ndd
 #'
@@ -33,75 +35,41 @@
 #'
 #' @export
 
-compute_ndd <- function(dataset1 = NULL, decision = NULL) {
-  freq_2 <- freq_1 <- dose_2 <- dose_1 <- DF_mean <- DF_min <- DF_max <- DN_mean <- DN_min <- DN_max <- optional <- NULL
-  # crate a dataset from dataset1 with unique dossageid
-  temp_presc <- dataset1[
-    !duplicated(dataset1[, c("dossageid")]),
-    c("dossageid", "text")
-  ]
-  temp_presc <- cbind(temp_presc[, "dossageid"], doseminer::extract_from_prescription(temp_presc$text) %>%
-    splitstackshape::cSplit(c("freq", "dose"), sep = "-", direction = "wide") %>%
-    dplyr::rowwise() %>% dplyr::mutate(
-      freq_2 = ifelse(is.na(freq_2), freq_1, freq_2),
-      dose_2 = ifelse(is.na(dose_2), dose_1, dose_2),
-      DF_mean = mean(c(freq_1, freq_2), na.rm = T),
-      DN_mean = mean(c(dose_1, dose_2), na.rm = T),
-      DF_min = freq_1, DF_max = freq_2,
-      DN_min = dose_1, DN_max = dose_2
-    ) %>%
-    dplyr::select(c(optional, DF_mean:DN_max)))
-  names(temp_presc)[1] <- "dossageid"
-  dataset1 <- dplyr::left_join(dataset1, temp_presc, by = "dossageid")
+compute_ndd <- function(data = NULL, decision = NULL) {
+  # Extract dose and frequency from free text.
+  temp_presc <- data %>%
+    dplyr::distinct(text) %>%
+    dplyr::pull(text) %>%
+    doseminer::extract_from_prescription(.) %>%
+    tidyr::separate(dose, c('dose1', 'dose2'),
+                    sep = '-', convert = TRUE, fill = 'right') %>%
+    tidyr::separate(freq, c('freq1', 'freq2'),
+                    sep = '-', convert = TRUE, fill = 'right') %>%
+    dplyr::mutate(
+      dose2 = dplyr::coalesce(dose1, dose2),
+      freq2 = dplyr::coalesce(freq1, freq2),
+      DF_mean = (freq1 + freq2) / 2,
+      DN_mean = (dose1 + dose2) / 2,
+      DF_min = freq1, DF_max = freq2,
+      DN_min = dose1, DN_max = dose2) %>%
+    dplyr::select(text = raw, optional, DF_mean:DN_max) %>%
+    dplyr::right_join(data, by = 'text')
 
-  if (decision == "min_min") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_min * DF_min)
-  }
-  else if (decision == "min_mean") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_min * DF_mean)
-  }
-  else if (decision == "min_max") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_min * DF_max)
-  }
-  else if (decision == "mean_min") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_mean * DF_min)
-  }
-  else if (decision == "mean_mean") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_mean * DF_mean)
-  }
-  else if (decision == "mean_max") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_mean * DF_max)
-  }
-  else if (decision == "max_min") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_max * DF_min)
-  }
-  else if (decision == "max_mean") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_max * DF_mean)
-  }
-  else if (decision == "max_max") {
-    dataset1 <- dataset1 %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(ndd = DN_max * DF_max)
-  }
-  dataset1 <- dataset1 %>%
-    dplyr::ungroup() %>% # NB 'rowwise()' is often very inefficient
-    dplyr::select(-matches("ndd\\d|^DN|^DF")) # get rid of DN_x, DF_x and ndd# cols
+  decision <- match.arg(decision, c('min_min', 'min_mean', 'min_max',
+                                    'mean_min', 'mean_mean', 'mean_max',
+                                    'max_min', 'max_mean', 'max_max'))
 
-  return(dataset1)
+  # Decide how to compute numeric daily dose.
+  temp_presc %>%
+    dplyr::mutate(ndd = switch(decision,
+                               min_min  = DN_min * DF_min,
+                               min_mean = DN_min * DF_max,
+                               min_max  = DN_min * DF_max,
+                               mean_min = DN_mean * DF_min,
+                               mean_mean = DN_mean * DF_mean,
+                               mean_max = DN_mean * DF_max,
+                               max_min  = DN_max * DF_min,
+                               max_mean = DN_max * DF_mean,
+                               max_max  = DN_max * DF_max)) %>%
+    dplyr::select(-matches('ndd\\d|^DN|^DF')) # get rid of DN_x, DF_x and ndd# cols
 }
