0be5f4b720b7955785ca6afda23046e66439b777
Selby <david@selbys.org.uk>
2021-06-15 13:15:58 +0100

    Rename function for checking plausible values

--- R/implausible_values.R
+++ R/implausible_values.R
@@ -1,18 +1,25 @@
-#' Define Implausible
+#' Check plausibility of doses and medication quantities
 #'
-#' This function flag quantity and ndd informations that are beyond the acceptable levels in the dataset.
-#' The acceptable level is obtained from NICE BNF.
+#' This function flags quantity and ndd data that are outside specified acceptable ranges.
+#' The acceptable level may be obtained from NICE BNF, or provided manually.
 #'
 #' @param data a data frame object containing the prescription information
 #' @param min_max_dat a data frame object containing minimum and maximum possible values for each drug
-#' @importFrom rlang .data
 #' @importFrom magrittr %>%
+#' @importFrom dplyr left_join mutate select
 #'
-#' @return Dataframe with the same structure as the input.
+#' @return A data frame with the same structure as the input, plus two columns:
+#' \itemize{
+#' \item{\code{implausible_qty}}
+#' \item{\code{implausible_ndd}}
+#' },
+#' both logical vectors indicating whether, respectively, the drug quantity or
+#' numeric daily dose are outside the plausible ranges given by \code{min_max_dat}.
+#' If there is no \code{prodcode} match to \code{min_max_dat}, plausibility is
+#' given as \code{NA}.
 #'
 #' @export
-
-Implausible_values <- function(data = NULL, min_max_dat = NULL) {
+check_implausible <- function(data = NULL, min_max_dat = NULL) {
   if (is.null(data)) {
     stop("\n Data set with information from therapy file of CPRD has to be supplimented")
   }
@@ -21,7 +28,7 @@ Implausible_values <- function(data = NULL, min_max_dat = NULL) {
   }
 
   data %>%
-    dplyr::left_join(min_max_dat, by = "prodcode")
+    dplyr::left_join(min_max_dat, by = "prodcode") %>%
     dplyr::mutate(
       implausible_qty = (qty > qty_max | qty < qty_min) & !is.na(qty),
       implausible_ndd = (ndd > max_rec_ndd | ndd < min_rec_ndd) & !is.na(ndd)
