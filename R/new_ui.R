ba24075170bb3f9091ebf4f4f79bbf8a9c419f35
David Selby <david.selby@manchester.ac.uk>
2021-09-21 16:25:10 +0100

    Add utility function for identifying implausible values

--- R/new_ui.R
+++ R/new_ui.R
@@ -12,7 +12,22 @@
 
 #' Impute missing or implausible values
 #' @import dplyr
-#' @param dataset A data frame containing columns \code{prodcode}, \code{pracid}, \code{patid}, \code{qty}
+#' @param dataset A data frame containing columns \code{prodcode}, \code{pracid}, \code{patid}
+#' @param variable Unquoted name of the column in \code{dataset} to be imputed
+#' @param which_fun Function applied to \code{variable} that returns a logical vector indicating which elements to impute. Defaults to \code{\link[base]{is.na}}
+#' @param fun Method for imputing the values. See details.
+#' @param group Level of structure for imputation. Defaults to whole study population.
+#' @param ... Not used
+#'
+#' @examples
+#' iris2 <- iris
+#' iris2[sample(nrow(iris), 50), 'Petal.Length'] <- NA
+#' colnames(iris2)[colnames(iris2) == 'Species'] <- 'prodcode'
+#' iris2$pracid <- as.numeric(as.numeric(iris2$prodcode) < 2)
+#' impute(iris2, Petal.Length, fun = 'median')
+#' impute(iris2, Petal.Length, fun = 'mean', group = 'population')
+#' impute(iris2, Petal.Length, fun = 'mean', group = 'pracid')
+#'
 #' @return A data frame of the same structure as \code{dataset}, with values imputed
 impute <- function(dataset,
                    variable,
@@ -40,10 +55,19 @@ impute <- function(dataset,
                                            {{ variable }}))
 }
 
-iris2 <- iris
-iris2[sample(nrow(iris), 50), 'Petal.Length'] <- NA
-colnames(iris2)[colnames(iris2) == 'Species'] <- 'prodcode'
-iris2$pracid <- as.numeric(as.numeric(iris2$prodcode) < 2)
-impute(iris2, Petal.Length, fun = 'median')
-impute(iris2, Petal.Length, fun = 'mean', group = 'population')
-impute(iris2, Petal.Length, fun = 'mean', group = 'pracid')
+#' Do values fall outside a specified 'plausible' range?
+#'
+#' A utility function for indicating if elements of a vector are implausible.
+#'
+#' Though the function \code{%within%} already exists, it is not vectorised.
+#'
+#' @param x numeric vector
+#' @param lower minimum plausible value
+#' @param upper maximum plausible value
+#' @param closed logical. If \code{TRUE}, values exactly equal to \code{lower} or \code{upper} are also considered implausible
+outside_range <- function(x, lower, upper, open = TRUE) {
+  if (closed) {
+    x < lower | x > upper
+  } else x <= lower | x >= upper
+}
+
