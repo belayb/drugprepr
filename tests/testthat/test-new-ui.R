f84d8a43e0ff06c8b53354bc5873d329e108ed0a
David Selby <david.selby@manchester.ac.uk>
2021-09-28 16:36:39 +0100

    Copy tests for ndd

--- tests/testthat/test-new-ui.R
+++ tests/testthat/test-new-ui.R
@@ -17,10 +17,25 @@ test_that('Replacing missing quantities with NA is equivalent to identity', {
                     identity(example_therapy))
 })
 
+test_that('Replacing missing numerical daily doses with NA is equivalent to identity', {
+  expect_equivalent(impute_ndd(example_therapy, 'ignore'),
+                    identity(example_therapy))
+  expect_equivalent(impute_ndd(example_therapy, 'missing'),
+                    identity(example_therapy))
+  expect_equivalent(impute_ndd(example_therapy, 'ignore', function(x) TRUE),
+                    identity(example_therapy))
+  expect_equivalent(impute_ndd(example_therapy, 'ignore', function(x) rep(TRUE, length(x))),
+                    identity(example_therapy))
+})
+
 test_that('Impute function does what it says on the tin', {
   library(dplyr)
   expect_equivalent(impute_qty(example_therapy, 'mean'),
                     example_therapy %>%
                       group_by(prodcode) %>%
                       mutate(qty = ifelse(is.na(qty), mean(qty, na.rm = TRUE), qty)))
+  expect_equivalent(impute_ndd(example_therapy, 'mean'),
+                    example_therapy %>%
+                      group_by(prodcode) %>%
+                      mutate(ndd = ifelse(is.na(ndd), mean(ndd, na.rm = TRUE), ndd)))
 })
